67a929a1c16b351ed149e152a2fae66fe6661196
vini <vini@VFIEVOX3.Router>
2025-09-28 13:15:16 -0400

    first commit project trivia on python

---
+++ project1.py
@@ -0,0 +1,29 @@
+#List of questions
+# store the answers
+# randomly pick questions
+# ask the questions to the user
+# see if they are correct 
+#keep the socre 
+# tell the user their score
+
+import random
+
+questions = {
+     "What keyword is used to define a function in Python?": "def",
+     "What is the output of len(\"hello\")?": "5",
+     "Which data type is used to store a sequence of items in a specific order and is mutable?": "list",
+     "What symbol is used to start a comment in Python?": "#",
+     "What will type(3.14) return?": "<class 'float'>",
+     "What keyword is used to handle exceptions in Python?": "try",
+     "Which built-in function is used to get user input from the console?": "input()",
+     "What is the boolean value of bool([])?": "False",
+     "What is the output of print(2 ** 3)?": "8",
+     "Which module is commonly used for working with regular expressions in Python?": "re ",
+}
+
+def python_trivia_game():
+    print("Welcome to the Python Trivia Game!")
+
+
+python_trivia_game()    
+python_trivia_game()  
