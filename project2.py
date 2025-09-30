b388d0d0098e38cbe1a03607954de3d1d414cc84
ViniciusAdams <viniciusadams98@gmail.com>
2025-09-30 20:25:19 +0100

    adding steps to the password generator

---
+++ project2.py
@@ -0,0 +1,8 @@
+#collect user preferences
+# lenght
+# should contain uppercase
+# should contain special
+# should contain digits
+
+# get all available characthers 
+# randomly pick characthers up to the lenght 
