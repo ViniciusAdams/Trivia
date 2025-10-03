2173395a846a003906e86d9a4e235dda31891525
ViniciusAdams <viniciusadams98@gmail.com>
2025-10-03 17:46:52 +0100

    finishing the trivia game

--- project2.py
+++ project2.py
@@ -14,16 +14,39 @@ import random
 import string
 
 def generate_password():
-    lenght = int(input("Enter the disere password lenght").strip())
-    include_uppercase = input("Include uppercase letters? (yes/no)").strip().lower()
-    include_special = input("Include special letters? (yes/no)").strip().lower()
-    include_digitals = input("Include uppercase letters? (yes/no)").strip().lower()
+    lenght = int(input("Enter the disere password lenght:").strip())
+    include_uppercase = input("Include uppercase letters? (yes/no)=").strip().lower()
+    include_special = input("Include special letters? (yes/no)=").strip().lower()
+    include_digits = input("Include uppercase letters? (yes/no)=").strip().lower()
 
     if lenght < 4:
         print ("Password lenght must be at least 4 characthers")
         return 
 
     lower = string.ascii_lowercase
-    print(lower)
+    uppercase = string.ascii_uppercase if include_uppercase == "yes" else ""
+    special = string.punctuation if include_special == "yes" else ""
+    digits = string.digits if include_digits == "yes" else ""
+    all_characters = lower + uppercase + special + digits
 
-generate_password()   
+    required_characters = []
+    if include_uppercase == "yes":
+        required_characters.append(random.choice(uppercase))
+    if include_special == "yes":
+        required_characters.append(random.choice(special))
+    if include_digits == "yes":
+        required_characters.append(random.choice(digits))
+
+    remaining_lenght = lenght - len(required_characters)
+    password = required_characters
+
+    for _ in range(remaining_lenght):
+        character = random.choice(all_characters)
+        password.append(character)
+
+    random.shuffle(password)
+    str_password = "".join(password)
+    return str_password
+
+password = generate_password()
+print(password) 
---
+++ project4.py
@@ -0,0 +1,10 @@
+#Load existing items
+#create a new item
+#list items
+#mark item as complete
+#save items
+
+def load_taks():
+    pass
+
+def save_taks():
