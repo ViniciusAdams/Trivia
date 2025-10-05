922d7b98811ee8df4e3bcb39e758ac00f1b2c957
ViniciusAdams <viniciusadams98@gmail.com>
2025-10-05 20:33:46 +0100

    pop method

--- Python_Crash_Course/chapter3.py
+++ Python_Crash_Course/chapter3.py
@@ -38,3 +38,17 @@ print(football_teams)
 
 del football_teams[0]
 print(football_teams)
+
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+#pop()
+popped_motorcycle = motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycle)
+
+motorcycles = ['honda','yamaha','suzuki']
+last_owned = motorcycles.pop()
+print(f"The last motorcycle I owned was a {last_owned.title()}.")
+
+first_owned = motorcycles.pop(0)
+print(f"The first motorcycle I owned was a {first_owned.title()}")
